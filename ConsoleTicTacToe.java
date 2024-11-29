303c5eb85ec7e2cf9b479184060947b41825902b
CaGListRo <paco.nyns@gmail.com>
2024-11-29 17:39:42 +0100

    WIP: Finished the game with winning conditions, but the tie is still missing.

--- ConsoleTicTacToe.java
+++ ConsoleTicTacToe.java
@@ -7,14 +7,81 @@ public class ConsoleTicTacToe {
         return currentPlayer *= -1;
     }
 
+    boolean checkWinning(int[][] logicBoard) {
+        // check rows and columns
+        for (int i = 0; i < 3; i++) {
+            if (logicBoard[0][i] + logicBoard[1][i] + logicBoard[2][i] == 3) {
+                return true;
+            }
+            if (logicBoard[0][i] + logicBoard[1][i] + logicBoard[2][i] == -3) {
+                return true;
+            }
+            if (logicBoard[i][0] + logicBoard[i][1] + logicBoard[i][2] == 3) {
+                return true;
+            }
+            if (logicBoard[i][0] + logicBoard[i][1] + logicBoard[i][2] == -3) {
+                return true;
+            }
+        }
+        // check diagonals
+        if (logicBoard[0][0] + logicBoard[1][1] + logicBoard[2][2] == 3) {
+            return true;
+        }
+        if (logicBoard[0][0] + logicBoard[1][1] + logicBoard[2][2] == -3) {
+            return true;
+        }
+        if (logicBoard[0][2] + logicBoard[1][1] + logicBoard[2][0] == 3) {
+            return true;
+        }
+        if (logicBoard[0][2] + logicBoard[1][1] + logicBoard[2][0] == -3) {
+            return true;
+        }
+        return false;
+    }
+
+    void drawBoard(char[][] visibleBoard, int[][] logicBoard) {
+        System.out.print("\033[H\033[2J"); // ANSI-Sequence to clear the console
+        System.out.flush();
+        // Print the visible board
+        for (int row = 0; row < visibleBoard.length; row++) {
+            for (int col = 0; col < visibleBoard[row].length; col++) {
+                switch (logicBoard[row][col]) {
+                    case 1 ->
+                        System.out.print("X");
+                    case -1 ->
+                        System.out.print("O");
+                    default ->
+                        System.out.print(visibleBoard[row][col]);
+                }
+
+                if (col != 2) {
+                    System.out.print(" | ");
+                } else {
+                    System.out.println();
+                }
+            }
+            if (row != 2) {
+                System.out.println("----------");
+            }
+        }
+    }
+
+    public int getPlayerNumber(int currentPlayer) {
+        if (currentPlayer == 1) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
     public static void main(String[] args) {
+        ConsoleTicTacToe game = new ConsoleTicTacToe();
         // Array declaration
         char[][] visibleBoard = new char[3][3];
         int[][] logicBoard = new int[3][3];
         // declaring variables
-        char playerOne = 'X';
-        char playerTwo = 'O';
         int currentPlayer = 1;
+        int playerNumber;
         int choice = 0;
         boolean gameOver = false;
         char number = '1';
@@ -31,46 +98,39 @@ public class ConsoleTicTacToe {
         }
 
         while (!gameOver) {
-            System.out.print("\033[H\033[2J"); // ANSI-Sequenz to clear the console
-            System.out.flush();
-            // Print the visible board
-            for (int row = 0; row < visibleBoard.length; row++) {
-
-                for (int col = 0; col < visibleBoard[row].length; col++) {
-                    if (logicBoard[row][col] == 1) {
-                        System.out.print(playerOne);
-                    } else if (logicBoard[row][col] == -1) {
-                        System.out.print(playerTwo);
-                    } else {
-                        System.out.print(visibleBoard[row][col]);
-                    }
-
-                    if (col != 2) {
-                        System.out.print(" | ");
-                    } else {
-                        System.out.println();
-                    }
-                }
-                if (row != 2) {
-                    System.out.println("----------");
-                }
-            }
-            while (choice == 0) {
-                int playerNumber;
-                if (currentPlayer == 1) {
-                    playerNumber = 1;
-                } else {
-                    playerNumber = 2;
-                }
+            // Draw the board
+            game.drawBoard(visibleBoard, logicBoard);
+            // get the real player number 1 or 2 instead of 1 and -1
+            playerNumber = game.getPlayerNumber(currentPlayer);
+            // get the player input
+            while (true) {
                 System.out.printf("Player %d enter number (1-9): ", playerNumber);
-                if (scanner.hasNextInt() && scanner.nextInt() >= 1 && scanner.nextInt() <= 9) {
+                if (scanner.hasNextInt()) {
                     choice = scanner.nextInt();
+                    if (choice < 1 || choice > 9) {
+                        System.out.println("Invalid input. Please enter a number between 1 and 9.");
+                    } else {
+                        break;
+                    }
                 } else {
-                    System.out.println("Invalid input. Please enter a number.");
+
+                    choice = 0;
                 }
             }
+            choice--;
+            logicBoard[(int) choice / 3][(int) choice % 3] = currentPlayer;
+            gameOver = game.checkWinning(logicBoard);
             choice = 0;
+            if (!gameOver) {
+                currentPlayer = game.swabPlayers(currentPlayer);
+            } else {
+                game.drawBoard(visibleBoard, logicBoard);
+                playerNumber = game.getPlayerNumber(currentPlayer);
+                System.out.printf("Game Over. Player %d won!%n", playerNumber);
+                break;
+            }
 
         }
+        scanner.close();
     }
 }
